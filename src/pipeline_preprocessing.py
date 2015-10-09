7ce1da1cf0b2bea5a6ec868d621510a1c5203a89
Stacey Terman <seterman@mit.edu>
2015-10-09 16:23:29 -0400

    Don't blow up on failures

--- src/pipeline_preprocessing.py
+++ src/pipeline_preprocessing.py
@@ -14,11 +14,13 @@ from pipeline_default_functions import make_default_finalizer
 # from pipeline_default_functions import format_as_html as defaultFormatter
 defaultFormatter = None
 
-stopOnError = True
-skippedSolutions = []
+stopOnError = False
+# skippedSolutions = []
 
 def tidy(tidyFn, dataSrc, tidyDest, testedFunctionName):
     ensure_folder_exists(tidyDest)
+
+    skipped = []
     print "Tidying data"
     for filename in os.listdir(dataSrc):
         if not filename.endswith('.py'):
@@ -31,10 +33,13 @@ def tidy(tidyFn, dataSrc, tidyDest, testedFunctionName):
             tidyFn(filename, dataSrc, tidyDest, testedFunctionName)
         except:
             if stopOnError: raise
-            skippedSolutions.append(solNum)
+            skipped.append(solNum)
+    return skipped
 
 def format(formatFn, dataSrc, formatDest):
     ensure_folder_exists(formatDest)
+    skipped = []
+
     print "Formatting data"
     for filename in os.listdir(dataSrc):
         solNum = filename.split('.')[0]
@@ -44,7 +49,8 @@ def format(formatFn, dataSrc, formatDest):
             formatFn(filename, dataSrc, formatDest)
         except:
             if stopOnError: raise
-            skippedSolutions.append(solNum)
+            skipped.append(solNum)
+    return skipped
 
 def add_test_case(testCase, dataSrc, testCaseDest):
     ensure_folder_exists(testCaseDest)
@@ -55,8 +61,10 @@ def add_test_case(testCase, dataSrc, testCaseDest):
 
 
 def run_logger(dataSrc, pickleDest, finalizer, traceMunger):
-    print "Running logger"
     ensure_folder_exists(pickleDest)
+    skipped = []
+
+    print "Running logger"
     for filename in os.listdir(dataSrc):
         solNum = filename.split('.')[0]
         print solNum
@@ -78,8 +86,15 @@ def run_logger(dataSrc, pickleDest, finalizer, traceMunger):
         # pprint.pprint(toPickle['trace'])
         toPickle['trace'] = traceMunger(toPickle['trace'])
 
-        with open(path.join(pickleDest, solNum + '.pickle'), 'w') as f:
-            pickle.dump(toPickle, f)
+        pickleFilePath = path.join(pickleDest, solNum + '.pickle')
+        try:
+            with open(pickleFilePath, 'w') as f:
+                pickle.dump(toPickle, f)
+        except pickle.PicklingError:
+            if stopOnError: raise
+            os.remove(pickleFilePath)
+            skipped.append(solNum)
+    return skipped
 
 def preprocess_pipeline_data(folderOfData,
                              testCase,
@@ -96,10 +111,24 @@ def preprocess_pipeline_data(folderOfData,
     if finalizer == None:
         finalizer = make_default_finalizer(testedFunctionName)
 
-    if tidier: tidy(tidier, folderOfData, tidyDataPath, testedFunctionName)
-    if formatter: format(formatter, tidyDataPath, formatPath)
-    if testCase: add_test_case(testCase, tidyDataPath, testCaseDataPath)
-    run_logger(testCaseDataPath, picklePath, finalizer=finalizer, traceMunger=traceMunger)
+    skipped_tidy = []
+    skipped_format = []
+    skipped_logger = []
+    if tidier:
+        skipped_tidy = tidy(tidier, folderOfData, tidyDataPath, testedFunctionName)
+    if formatter:
+        skipped_format = format(formatter, tidyDataPath, formatPath)
+    if testCase:
+        add_test_case(testCase, tidyDataPath, testCaseDataPath)
+    skipped_logger = run_logger(testCaseDataPath, picklePath, finalizer=finalizer, traceMunger=traceMunger)
 
-    print "Solutions skipped:", len(skippedSolutions)
-    pprint.pprint(skippedSolutions, indent=2)
+    print "Solutions skipped:", len(skipped_tidy) + len(skipped_format) + len(skipped_logger)
+    if skipped_tidy:
+        print "SKIPPED BY TIDIER:"
+        pprint.pprint(skipped_tidy, indent=2)
+    if skipped_format:
+        print "SKIPPED BY FORMATTER:"
+        pprint.pprint(skipped_format, indent=2)
+    if skipped_logger:
+        print "SKIPPED BY LOGGER:"
+        pprint.pprint(skipped_logger, indent=2)
