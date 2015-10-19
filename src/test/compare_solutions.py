0997dd8f1468ef26a2907f7ad1cb7af05cbcb9e8
Stacey Terman <seterman@mit.edu>
2015-10-18 22:03:13 -0400

    Add function to compare solutions.json files

---
+++ src/test/compare_solutions.py
@@ -0,0 +1,27 @@
+import json
+import pprint
+import sys
+
+def compare(path1, path2):
+    with open(path1, 'r') as f:
+        data1 = json.load(f)
+    with open(path2, 'r') as f:
+        data2 = json.load(f)
+
+    def extract_members(sol):
+        return frozenset(sol['members'])
+
+    sets1 = set(map(extract_members, data1))
+    sets2 = set(map(extract_members, data2))
+    return (sets1 - sets2, sets2 - sets1)
+
+def pretty_print(path1, path2):
+    in1not2, in2not1 = compare(path1, path2)
+    print "In 1 not 2:"
+    pprint.pprint(map(list, list(in1not2)))
+    print "******"
+    print "In 2 not 1:"
+    pprint.pprint(map(list, list(in2not1)))
+
+if __name__ == '__main__':
+    pretty_print(sys.argv[1], sys.argv[2])
