77832d3a1f59d271f333213bf81922b5b5068d59
eglassman <elena.glassman@gmail.com>
2016-01-27 13:42:37 -0500

    clicking on correct sol brings up closest incorrect solutions

--- overcode-meteor/client/client.js
+++ overcode-meteor/client/client.js
@@ -12,9 +12,26 @@ Template.solution.helpers({
     }
 });
 
+Template.incorrectSolutionsList.helpers({
+    "closestToClickedCorrect": function(closest_stacks){
+        // console.log(closest_stacks)
+        // return true
+        var clickedStack = Session.get('clickedCorrectStack');
+        return closest_stacks.indexOf(clickedStack)>=0
+    }
+})
+
 Template.registerHelper('log',function(){
     console.log('template logging',this);
 });
 Template.registerHelper('solutions',function(){
     return Stacks.find({}).fetch();
 });
+
+Template.solution.events({
+    "click .correct": function(event){
+        console.log('correct solution clicked',event,event.currentTarget)
+        var clickedCorrectStackID = parseInt($(event.currentTarget).prop('id'));
+        Session.set('clickedCorrectStack',clickedCorrectStackID)
+    }
+})
