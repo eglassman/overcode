945ccd3a175c512eb8d873cdb8e310430154f527
Stacey Terman <seterman@mit.edu>
2016-01-28 13:04:31 -0500

    Pin clicked stack and filter

--- overcode-meteor/client/client.js
+++ overcode-meteor/client/client.js
@@ -11,10 +11,39 @@ Template.solution.helpers({
         return " ".repeat(indent);
     },
     "clicked": function(stackID){
-        return stackID==Session.get('clickedCorrectStack')
+        return stackID==Session.get('clickedStackID')
     },
     "sharedWithClickedStack": function(phraseID){
+        var referenceIDs = Session.get('clickedStackPhraseIDs');
+        if (referenceIDs === undefined) {
+            return false;
+        }
         return Session.get('clickedStackPhraseIDs').indexOf(phraseID) >= 0
+    }
+});
+
+Template.solutionsList.helpers({
+    "solutions": function() {
+        var stackID = Session.get('clickedStackID');
+        if (stackID === undefined) {
+            return Stacks.find({}, { sort: {'count': -1} }).fetch();
+        }
+        return Stacks.find({ $or: [
+                { id: stackID },
+                // stupid hack: Meteor doesn't support $eq, apparently, so use
+                // $in instead
+                { closest_stacks: {$elemMatch: {$in: [stackID]}} }
+        ]}, { sort: {'count': -1} }).fetch();
+    }
+});
+
+Template.pinnedStack.helpers({
+    "getStack": function() {
+        var stackID = Session.get('clickedStackID');
+        if (stackID === undefined) {
+            return;
+        }
+        return Stacks.findOne({ id: stackID });
     }
 });
 
@@ -29,9 +58,10 @@ Template.incorrectSolutionsList.helpers({
         return Stacks.find({}, { sort: {'count_closest_stacks': 1} }).fetch();
     },
     "closestToClickedCorrect": function(closest_stacks){
-        // console.log(closest_stacks)
-        // return true
-        var clickedStack = Session.get('clickedCorrectStack');
+        var clickedStack = Session.get('clickedStackID');
+        if (clickedStack === undefined) {
+            return true;
+        }
         return closest_stacks.indexOf(clickedStack)>=0
     }
 });
@@ -41,12 +71,22 @@ Template.registerHelper('log',function(){
 });
 
 Template.solution.events({
-    "click .correct": function(event){
-        console.log('correct solution clicked',event,event.currentTarget)
-        var clickedCorrectStackID = parseInt($(event.currentTarget).prop('id'));
+    "click .stack": function(event){
+        console.log('solution clicked')
+        var clickedStackID = parseInt($(event.currentTarget).prop('id'));
         var clickedStackPhraseIDs = $(event.currentTarget).data('phraseids');
-        clickedStackPhraseIDs = clickedStackPhraseIDs.split(',').map(function(x){return parseInt(x);});
-        Session.set('clickedCorrectStack',clickedCorrectStackID)
+        clickedStackPhraseIDs = clickedStackPhraseIDs.split(',').map(function(x) {
+            return parseInt(x);
+        });
+        Session.set('clickedStackID', clickedStackID)
         Session.set('clickedStackPhraseIDs', clickedStackPhraseIDs)
     }
-})
+});
+
+Template.pinnedStack.events({
+    "click .remove": function(event) {
+        console.log('pinned stack clicked');
+        Session.set('clickedStackID', undefined);
+        Session.set('clickedStackPhraseIDs', undefined);
+    }
+});
