6b127fa7798aacf900d078a44670c4fc4c52f7b1
Stacey Terman <seterman@mit.edu>
2016-02-22 13:46:11 -0500

    backend: add script for extracting from csv

---
+++ src/testcase_making/extract_solutions_final.py
@@ -0,0 +1,63 @@
+import csv
+import os
+import pprint
+import sys
+
+# exam schema: "username", "time", "grade", "attempts", "answer"
+def extract_solutions_from_file(filename, output_base):
+    max_id = 0
+    username_to_id = {}
+    num_correct = 0
+    num_incorrect = 0
+    total_num_lines = 0
+    total_num_attempts = 0
+
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        for (i, row) in enumerate(reader):
+            if i == 0:
+                # header row
+                continue
+            username, time, grade, attempts, student_response = row
+
+            if grade == "1.0":
+                num_correct += 1
+            else:
+                num_incorrect += 1
+
+            if username in username_to_id:
+                student_id = username_to_id[username]
+            else:
+                student_id = 'student_' + str(max_id)
+                username_to_id[username] = student_id
+                max_id += 1
+
+            try:
+                attempts = int(attempts)
+            except ValueError:
+                attempts = 1 # not sure why sometimes there's a row without this
+            total_num_attempts += attempts
+            total_num_lines += len(student_response.split('\n'))
+
+            extra_info = ("# attempts: %s" % attempts + "\n" +
+                "# grade: %s" % grade + "\n\n")
+            with open(os.path.join(output_base, str(i-1) + '.py'), 'w') as sol_file:
+                sol_file.write(extra_info + student_response)
+
+    print "Incorrect solutions/Total solutions:", str(num_incorrect) + '/' + str(num_incorrect + num_correct)
+    print "Number of unique students:", len(username_to_id)
+    print "Average number of attempts:", float(total_num_attempts) / len(username_to_id)
+    print "Average number of lines:", float(total_num_lines) / (num_incorrect + num_correct)
+
+    with open('mapping.txt', 'w') as f:
+        pprint.pprint(username_to_id, f)
+
+if __name__ == '__main__':
+    output_base = os.path.join(sys.argv[2], "data")
+
+    try:
+        os.makedirs(output_base)
+    except OSError:
+        pass
+
+    extract_solutions_from_file(sys.argv[1], output_base)
