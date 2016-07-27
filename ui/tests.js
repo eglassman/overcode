046953d734ca44d7851b8fab9add320135968f77
eglassman <elena.glassman@gmail.com>
2016-07-27 14:04:47 -0400

    updated UI unit tests

--- ui/tests.js
+++ ui/tests.js
@@ -11,3 +11,7 @@ QUnit.test('test renderFloatsTo2DecimalPlaces on integers', function(assert){
   var testStr = '1';
   assert.ok( renderFloatsTo2DecimalPlaces(testStr)==1, "Passed!");
 });
+
+// QUnit.test('test varNameComparator', function(assert){
+//   assert.ok( varNameComparator({varName:'a___2'},{varName:'a___10'})<0, "Passed!");
+// });
